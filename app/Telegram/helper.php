52f5e95beb67ecd151dd1146259480f0376f0d46
Jenno Ramcaj <jennoramcaj@gmail.com>
2026-02-24 15:47:28 +0100

    replace dot

--- app/Telegram/helper.php
+++ app/Telegram/helper.php
@@ -5,6 +5,7 @@ use SergiX44\Nutgram\Telegram\Types\User\User;
 if (! function_exists('buildUserMention')) {
     function buildUserMention(User $user): string
     {
-        return "[{$user->first_name}](tg://user?id={$user->id})";
+        $firstName = \Illuminate\Support\Str::replace('.', '', $user->first_name);
+        return "[{$firstName}](tg://user?id={$user->id})";
     }
 }
